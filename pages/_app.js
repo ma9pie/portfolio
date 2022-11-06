03afb8eebffa2207a34d9ad5d7698b6887ced26f
ma9pie <ma9pie.dev@gmail.com>
2022-11-06 23:08:04 +0900

    구글 애널리틱스 연결

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,25 @@
+import styled from "@emotion/styled";
+import { useRouter } from "next/router";
+import React, { useEffect, useState } from "react";
 import "@/styles/reset.scss";
 import "@/styles/fonts.scss";
 import "@/styles/globals.scss";
 import "@/styles/simplebar.scss";
 
 function App({ Component, pageProps }) {
+  const router = useRouter();
   const getLayout = Component.getLayout || ((page) => page);
+
+  // 구글 애널리틱스 조회수 측정
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      GoogleAnalyticsUtils.changeRouteGtag(url);
+    };
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
 
   return getLayout(<Component {...pageProps} />);
 }
