d0a9120714c293668969100456961550d27e5b97
김준영 <kjy1787@newlinkcorp>
2022-11-07 15:21:09 +0900

    meta tag 수정

--- pages/_document.js
+++ pages/_document.js
@@ -4,7 +4,10 @@ export default function Document() {
   return (
     <Html>
       <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
+        />
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="mobile-web-app-capable" content="yes" />
         <link rel="manifest" href="/manifest.json" />
